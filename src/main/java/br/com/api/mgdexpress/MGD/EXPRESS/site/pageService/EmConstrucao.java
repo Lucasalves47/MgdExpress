f366f566e836c12db79a48694359fc9ec1c47e67
lucasalves747 <134sandrinha@gmail.com>
2024-01-02 10:39:31 -0300

    criando a

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/EmConstrucao.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/EmConstrucao.java
@@ -35,7 +35,7 @@ public class EmConstrucao {
                 <body>
                     <h1>Em Construção</h1>
                     <p>Esta página está em construção. Em breve estará disponível.</p>
-                    <img src="under-construction.png" alt="Em Construção">
+                    <img src="https://yata.s3-object.locaweb.com.br/aff51a54cbba5ae8e7c9c2cd95c8575927cfb1b3d15c6288555e002f4cbeb62a" alt="Em Construção">
                 </body>
                 </html>
                 """;
