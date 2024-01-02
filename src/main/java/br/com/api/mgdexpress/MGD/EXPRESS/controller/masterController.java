60de0440a950ada5f0caff57da70b559d3f9b445
lucasalves747 <134sandrinha@gmail.com>
2024-01-01 22:06:24 -0300

    criando a funcao de buscar motoboy por id

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/controller/masterController.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/controller/masterController.java
@@ -1,12 +1,14 @@
 package br.com.api.mgdexpress.MGD.EXPRESS.controller;
 
 import br.com.api.mgdexpress.MGD.EXPRESS.model.motoboy.DadosMotoboyList;
+import br.com.api.mgdexpress.MGD.EXPRESS.model.motoboy.Motoboy;
 import br.com.api.mgdexpress.MGD.EXPRESS.model.users.User;
 import br.com.api.mgdexpress.MGD.EXPRESS.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +46,13 @@ public class masterController {
 
         return ResponseEntity.ok().build();
     }*/
+
+    @PreAuthorize("hasRole('ROLE_USER_MASTER')")
+    @GetMapping("/motoboy/{id}")
+    public ResponseEntity<Motoboy> MotoboysPorId(@PathVariable Long id){
+        return ResponseEntity.ok(motoboyRepository.getReferenceById(id));
+    }
+
 
 
 }
