d4698269a1a407031eea5073bf2beb235b48b977
lucasalves747 <134sandrinha@gmail.com>
2024-01-02 02:42:41 -0300

    trocando os detalhes do site por uma pagina em construcao

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
@@ -175,7 +175,7 @@ public class MainHtml {
                                                        <p class="titulo"><strong>${cardData.nomePedido}</strong></p>
                                                        <p><strong>Valor:</strong> ${cardData.valor}</p>
                                                        <p><strong>Local de Destino:</strong> ${cardData.localDestino}</p>
-                                                       <a onclick="carregarPagina('${url}/pedido/detalhes/${cardData.id}')"><button>Detalhes</button></a>
+                                                       <a onclick="carregarPagina('${url}pedido/detalhes/${cardData.id}')"><button>Detalhes</button></a>
                                                    `;
                                       
                                                    cardContent.innerHTML = cardDetails;
