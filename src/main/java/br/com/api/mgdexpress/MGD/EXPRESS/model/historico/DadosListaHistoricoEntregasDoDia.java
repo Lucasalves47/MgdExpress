23d7d916a42d7b4545cd47b098e4b2ec012f2fc8
lucas2651 <134lucasalves@gmail.com>
2024-01-14 22:31:43 -0300

    add histolrico de entregas do motoboy add colun distancia

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/model/historico/DadosListaHistoricoEntregasDoDia.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/model/historico/DadosListaHistoricoEntregasDoDia.java
@@ -2,9 +2,9 @@ package br.com.api.mgdexpress.MGD.EXPRESS.model.historico;
 
 import java.math.BigDecimal;
 
-public record DadosListaHistoricoEntregasDoDia(String nomeMotoboy, BigDecimal valor,Integer entregas,Double km) {
+public record DadosListaHistoricoEntregasDoDia(Long id,String nomeMotoboy, BigDecimal valor,Integer entregas,Double km) {
 
     public DadosListaHistoricoEntregasDoDia(Historico historico,Integer entregas) {
-        this(historico.getMotoboy().getNome(),historico.getValor(),entregas, historico.getDistancia());
+        this(historico.getMotoboy().getId(),historico.getMotoboy().getNome(),historico.getValor(),entregas, historico.getDistancia());
     }
 }
