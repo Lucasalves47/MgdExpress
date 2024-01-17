80f463a0d06e64bca3a66f808199898a1dd725da
lucas2651 <134lucasalves@gmail.com>
2024-01-17 00:29:59 -0300

    add html do historico entregas

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Formulario.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Formulario.java
@@ -88,8 +88,8 @@ public class Formulario {
                         <textarea id="itensDoPedido" name="itensDoPedido" placeholder="Digite seu texto aqui..." rows="8" cols="50"></textarea>
                                        
                         <button type="button" onclick="enviarPedido()">Criar</button>
-                        <button style="background-color: #808080;margin-top: 38px;"  type="button" onclick="carregarPagina("https://mgdexpress-production.up.railway.app/site/gerente/home")">Cancelar</button>
                     </form>
+                    <button style="background-color: #808080;margin-top: 28px;"  type="button" onclick="carregarPagina("https://mgdexpress-production.up.railway.app/site/gerente/home")">Cancelar</button>
                 </body>
                 </html>
                                 
