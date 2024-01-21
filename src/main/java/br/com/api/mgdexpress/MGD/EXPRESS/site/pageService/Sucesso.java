fd83802fdf89e827d45d789d2c5817e99fb2e5fc
lucas2651 <134lucasalves@gmail.com>
2024-01-21 17:17:58 -0300

    atualizando a url

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Sucesso.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Sucesso.java
@@ -52,7 +52,7 @@ public class Sucesso {
                 <div class="container">
                     <h1>Pedido Criado com Sucesso!</h1>
                     <p>Seu pedido foi registrado com sucesso. Obrigado!</p>
-                    <button onclick="carregarPagina(''"""+url+"/site/gerente/home')"+"""
+                    <button onclick="carregarPagina('"""+url+"/site/gerente/home')"+"""
                 >Voltar para a Home</button>
                 </div>
                                 
