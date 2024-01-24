e75d45ff0ab95a5ae89d17ff9a237990ef4e161b
lucas2651 <134lucasalves@gmail.com>
2024-01-24 18:50:59 -0300

    html mapa

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Home.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Home.java
@@ -6,84 +6,13 @@ public class Home {
         return """
                 <head>
                     <title>MGD EXPRESS</title>
-                    <style>
-                        body {
-                            margin: 0;
-                            padding: 0;
-                            font-family: Arial, sans-serif;
-                        }
-                                
-                        nav {
-                            background-color: #333;
-                            color: white;
-                            padding: 10px;
-                            display: flex;
-                            justify-content: space-between;
-                            align-items: center;
-                        }
-                                
-                        nav h2 {
-                            margin: 0;
-                        }
-                                
-                        nav a {
-                            color: white;
-                            text-decoration: none;
-                            margin-left: 15px;
-                        }
-                                
-                        main {
-                            text-align: center;
-                            padding: 20px;
-                        }
-                                
-                        #map {
-                            width: 100%;
-                            height: 600px;
-                            /* Ajuste a altura conforme necessário */
-                        }
-                                
-                        nav button {
-                            background-color: #4CAF50;
-                            color: white;
-                            padding: 10px 20px;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                            font-size: 16px;
-                            margin-left: 15px;
-                        }
-                                
-                        nav button:hover {
-                            background-color: #45a049;
-                        }
-                    </style>
                 </head>
-                                
-                                
-                <nav>
-                    <h2>MGD EXPRESS</h2>
-                    <div>
-                        <button onclick="clearInterval(intervalId);carregarPagina('"""+url+"/site/gerente/criar')"+"""
-                            ">Novo Pedido</button>
-                        <button onclick="clearInterval(intervalId);listarPedidos();">Meus Pedidos</button>
-                        <button onclick="clearInterval(intervalId);listarHistoricoEntregas();">Entregas do dia</button>
-                       
-                        <button onclick="clearInterval(intervalId);listarHistorico();">Histórico</button>
-                    </div>
-                </nav>
-                                
-                                
-                <main>
-                    <div id="map"></div>
-                </main>
-                
+              
                 <script>
                     listarPedidos()
                     </script>
                     
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCsTWHMwA_agU_-o35U_3b606930nBrsY8&callback=initMap" async
-                    defer></script> """;
+                """;
     }
 }
 
--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/ListarMeusPedidos.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/ListarMeusPedidos.java
@@ -5,116 +5,83 @@ public class ListarMeusPedidos {
     public static String listar(String url){
         return """
                                
-                <head>
-                    <title>Meus Pedidos</title>
-                    <style>
-                        body {
-                            font-family: Arial, sans-serif;
-                            margin: 0;
-                            padding: 0;
-                            background-color: #f4f4f4;
-                        }
-                             
-                                   
-                        .card {
-                            border: 1px solid #ccc;
-                            border-radius: 8px;
-                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                            margin: 10px;
-                            width: 300px;
-                            float: left;
-                            background-color: #fff;
-                        }
-                                       
-                        .card img {
-                            width: 100%;
-                            height: 150px;
-                            object-fit: cover;
-                            border-top-left-radius: 8px;
-                            border-top-right-radius: 8px;
-                        }
-                                       
-                        .card-content {
-                            padding: 10px;
-                            text-align: left;
-                        }
-                        .titulo{
-                            text-align: center;
-                        }
-                                       
-                        button {
-                            background-color: #4caf50;
-                            color: #fff;
-                            padding: 8px;
-                            border: none;
-                            border-radius: 4px;
-                            cursor: pointer;
-                            width: 100%;
-                        }
-                                       
-                        button:hover {
-                            background-color: #45a049;
-                        }
-                                       
-                        @media (max-width: 768px) {
-                            .card {
-                                width: calc(50% - 20px);
-                            }
-                        }
-                                       
-                        nav a {
-                            color: white;
-                            text-decoration: none;
-                            margin-left: 15px;
-                        }
-                                       
-                        .backLink{
-                            font-size : 160%;
-                        }
-                        h2{margin-left: 40%;}
-                        
-                        nav button {
-                            background-color: #4CAF50;
-                            color: white;
-                            padding: 10px 20px;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                            font-size: 16px;
-                            margin-left: 15px;
-                        }
-                                
-                        nav button:hover {
-                            background-color: #45a049;
-                        }
-                        .toolbar {
-                            background-color: #333;
-                            color: white;
-                            padding: 10px;
-                            display: inline;
-                            justify-content: space-between;
-                            align-items: center;
-                            
-                        }
-                    </style>
-                </head>
-                                       
-                                       
-                <nav class="toolbar">
-               <h2>MGD EXPRESS</h2>
-               <div>
-                       <button class="btnNav" onclick="carregarPagina('"""+url+"/site/gerente/criar')"+"""
-                           ">Novo Pedido</button>
-                     
-                       <button class="btnNav" onclick="listarHistoricoEntregas();">Entregas do dia</button>
-                                       
-                       <button class="btnNav" onclick="listarHistorico();">Histórico</button>
-                   </div>
-               </nav>
-                                       
-                <div id="card-container"></div>
-                
-              
+                 <head>
+                                    <title>MGD EXPRESS</title>
+                                    <style>
+                                        body {
+                                            margin: 0;
+                                            padding: 0;
+                                            font-family: Arial, sans-serif;
+                                        }
+                                               
+                                        nav {
+                                            background-color: #333;
+                                            color: white;
+                                            padding: 10px;
+                                            display: flex;
+                                            justify-content: space-between;
+                                            align-items: center;
+                                        }
+                                               
+                                        nav h2 {
+                                            margin: 0;
+                                        }
+                                               
+                                        nav a {
+                                            color: white;
+                                            text-decoration: none;
+                                            margin-left: 15px;
+                                        }
+                                               
+                                        main {
+                                            text-align: center;
+                                            padding: 20px;
+                                        }
+                                               
+                                        #map {
+                                            width: 100%;
+                                            height: 600px;
+                                            /* Ajuste a altura conforme necessário */
+                                        }
+                                               
+                                        nav button {
+                                            background-color: #4CAF50;
+                                            color: white;
+                                            padding: 10px 20px;
+                                            border: none;
+                                            border-radius: 5px;
+                                            cursor: pointer;
+                                            font-size: 16px;
+                                            margin-left: 15px;
+                                        }
+                                               
+                                        nav button:hover {
+                                            background-color: #45a049;
+                                        }
+                                    </style>
+                                </head>
+                                               
+                                               
+                                <nav>
+                                    <h2>MGD EXPRESS</h2>
+                                    <div>
+                                        <button onclick="clearInterval(intervalId);carregarPagina('""\"+url+"/site/gerente/criar')"+""\"
+                                            ">Novo Pedido</button>
+                                        <button onclick="clearInterval(intervalId);listarPedidos();">Meus Pedidos</button>
+                                        <button onclick="clearInterval(intervalId);listarHistoricoEntregas();">Entregas do dia</button>
+                                      
+                                        <button onclick="clearInterval(intervalId);listarHistorico();">Histórico</button>
+                                    </div>
+                                </nav>
+                                               
+                                               
+                                <body>
+                                    <div id="card-container"></div>
+                                </body>
+                               
+                                <script>
+                                    listarPedidos()
+                                    </script>
                 """;
     }
 }
