3759f1ef23b7f880d366e594b910100ce22a76c7
lucas2651 <134lucasalves@gmail.com>
2024-01-25 14:09:43 -0300

    tirando o responsivel

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/DetalhePedido.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/DetalhePedido.java
@@ -8,129 +8,112 @@ public class DetalhePedido {
         return """
                 
                       <!DOCTYPE html>
-                      <html lang="pt-br">
-                      
-                      <head>
-                          <meta charset="UTF-8">
-                          <title>Detalhe do Histórico</title>
-                          <style>
-                              body {
-                                  font-family: Arial, sans-serif;
-                                  margin: 0;
-                                  padding: 0;
-                                  background-color: #f4f4f4;
-                                  color: #333;
-                              }
-                      
-                              nav {
-                                  background-color: #333;
-                                  color: white;
-                                  padding: 10px;
-                                  text-align: center;
-                                  display: flex;
-                                  align-items: center;
-                              }
-                      
-                              main {
-                                  padding: 20px;
-                              }
-                      
-                              .card {
-                                  background-color: #fff;
-                                  border-radius: 8px;
-                                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                                  margin-bottom: 20px;
-                                  padding: 20px;
-                              }
-                      
-                              .card p {
-                                  margin: 0;
-                                  margin-bottom: 10px;
-                              }
-                      
-                              .card h3 {
-                                  margin-top: 0;
-                                  color: #4caf50;
-                              }
-                      
-                              .backLink {
-                                  cursor: pointer;
-                              }
-                      
-                              h2 {
-                                  margin-left: 40%;
-                                  color: #fff;
-                              }
-                      
-                              ul {
-                                  list-style-type: none;
-                                  padding: 0;
-                              }
-                      
-                              li {
-                                  margin-bottom: 10px;
-                              }
-                      
-                              /* Media Queries para tornar o layout responsivo */
-                              @media only screen and (max-width: 600px) {
-                                  h2 {
-                                      margin-left: 20px; /* Ajusta a margem esquerda do título */
-                                  }
-                      
-                                  .card {
-                                      padding: 15px; /* Reduz o preenchimento interno da carta */
-                                  }
-                              }
-                      
-                              @media only screen and (max-width: 400px) {
-                                  h2 {
-                                      margin-left: 10px; /* Ajusta a margem esquerda do título */
-                                  }
-                              }
-                          </style>
-                      </head>
-                      
-                      <body>
-                          <nav>
-                              <div>
-                                  <p class="backLink" onclick='listarPedidos()'>←</p>
-                              </div>
-                              <h2>Detalhe do Pedido</h2>
-                          </nav>
-                      
-                          <main>
-                              <div class="card">
-                                  <h3>Pedido</h3>
-                                  <p>Nome do Estabelecimento: """ + dados.getNomeEstabelecimento() + """
-                                  </p>
-                                  <p>Local de Origem : """ + dados.getLocalOrigem() + """
-                                  </p>
-                                  <p>Local de Destino : """ + dados.getLocalDestino() + """
-                                  </p>
-                                  <p>Valor : """ + dados.getValor() + """
-                                  </p>
-                                  <p>Observação : """ + dados.getObservacao() + """
-                                  </p>
-                                  <p>Itens do Pedido : """ + dados.getItensDoPedido() + """
-                                  </p>
-                                  <p>Data de Criação :  """ + dados.getDataCriacao() + """
-                                  </p>
-                      
-                                  <h3>Gerente:</h3>
-                                  <ul>
-                                      <li>Nome : """ + dados.getGerente().getNome() + """
-                                      </li>
-                                      <li>Telefone : """ + dados.getGerente().getTelefone() + """
-                                      </li>
-                                      <li>Email : """ + dados.getGerente().getEmail() + """
-                                  </li>
-                                  </ul>
-                              </div>
-                          </main>
-                      </body>
-                      
-                      </html>
-                      
+                              <html lang="pt-br">
+                              
+                              <head>
+                                  <meta charset="UTF-8">
+                                  <title>Detalhe do Histórico</title>
+                                  <style>
+                                      body {
+                                          font-family: Arial, sans-serif;
+                                          margin: 0;
+                                          padding: 0;
+                                          background-color: #f4f4f4;
+                                          color: #333;
+                                      }
+                              
+                                      nav {
+                                          background-color: #333;
+                                          color: white;
+                                          padding: 10px;
+                                          text-align: center;
+                                          display: flex;
+                                          align-items: center;
+                                      }
+                              
+                                      main {
+                                          padding: 20px;
+                                      }
+                              
+                                      .card {
+                                          background-color: #fff;
+                                          border-radius: 8px;
+                                          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                          margin-bottom: 20px;
+                                          padding: 20px;
+                                      }
+                              
+                                      .card p {
+                                          margin: 0;
+                                          margin-bottom: 10px;
+                                      }
+                              
+                                      .card h3 {
+                                          margin-top: 0;
+                                          color: #4caf50;
+                                      }
+                              
+                                      .backLink {
+                                          cursor: pointer;
+                                      }
+                              
+                                      h2 {
+                                          margin-left: 40%;
+                                          color: #fff;
+                                          
+                                      }
+                              
+                                      ul {
+                                          list-style-type: none;
+                                          padding: 0;
+                                      }
+                              
+                                      li {
+                                          margin-bottom: 10px;
+                                      }
+                                  </style>
+                              </head>
+                              
+                              <body>
+                                  <nav>
+                                      <div>
+                                          <p class="backLink" onclick='listarPedidos()'>←</p>
+                                      </div>
+                                      <h2>Detalhe do Pedido</h2>
+                                  </nav>
+                              
+                                  <main>
+                                      <div class="card">
+                                          <h3>Pedido</h3>
+                                          <p>Nome do Estabelecimento: """ + dados.getNomeEstabelecimento() + """
+                                          </p>
+                                          <p>Local de Origem : """ + dados.getLocalOrigem() + """
+                                          </p>
+                                          <p>Local de Destino : """ + dados.getLocalDestino() + """
+                                          </p>
+                                          <p>Valor : """ + dados.getValor() + """
+                                          </p>
+                                          <p>Observação : """ + dados.getObservacao() + """
+                                          </p>
+                                          <p>Itens do Pedido : """ + dados.getItensDoPedido() + """
+                                          </p>
+                                          <p>Data de Criação :  """ + dados.getDataCriacao() + """
+                                          </p>
+                           
+                                          <h3>Gerente:</h3>
+                                          <ul>
+                                              <li>Nome : """ + dados.getGerente().getNome() + """
+                                              </li>
+                                              <li>Telefone : """ + dados.getGerente().getTelefone() + """
+                                              </li>
+                                              <li>Email : """ + dados.getGerente().getEmail() + """
+                                          </li>
+                                          </ul>
+                                      </div>
+                                  </main>
+                              </body>
+                              
+                              </html>
                               
                 """;
     }
