11af47c1717ea1769dcb8d1d8bfa0ccb24e1ce7c
lucas2651 <134lucasalves@gmail.com>
2024-01-30 00:38:28 -0300

    mudando a home conforme solicitado pelo cliente

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Home.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/Home.java
@@ -7,56 +7,101 @@ public class Home {
                 <head>
                     <title>MGD EXPRESS</title>
                     <style>
-                        body {
-                            margin: 0;
-                            padding: 0;
-                            font-family: Arial, sans-serif;
-                        }
-                                
-                        nav {
-                            background-color: #333;
-                            color: white;
-                            padding: 10px;
-                            display: flex;
-                            justify-content: space-between;
-                            align-items: center;
-                        }
-                                
-                        nav h2 {
-                            margin: 0;
-                        }
-                                
-                        nav a {
-                            color: white;
-                            text-decoration: none;
-                            margin-left: 15px;
-                        }
-                                
-                        main {
-                            text-align: center;
-                            padding: 20px;
-                        }
-                                
-                        #map {
-                            width: 100%;
-                            height: 600px;
-                            /* Ajuste a altura conforme necessário */
-                        }
-                                
-                        nav button {
-                            background-color: #4CAF50;
-                            color: white;
-                            padding: 10px 20px;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                            font-size: 16px;
-                            margin-left: 15px;
-                        }
-                                
-                        nav button:hover {
-                            background-color: #45a049;
-                        }
+                       body {
+                                     margin: 0;
+                                     padding: 0;
+                                     font-family: Arial, sans-serif;
+                                 }
+                         
+                                 nav {
+                                     background-color: #333;
+                                     color: white;
+                                     padding: 10px;
+                                     display: flex;
+                                     justify-content: space-between;
+                                     align-items: center;
+                                 }
+                         
+                                 nav h2 {
+                                     margin: 0;
+                                 }
+                         
+                                 nav a {
+                                     color: white;
+                                     text-decoration: none;
+                                     margin-left: 15px;
+                                 }
+                         
+                                 main {
+                                     margin-top: 0%;
+                                 }
+                         
+                                 #map {
+                                     width: 100%;
+                                     height: 92%;
+                                 }
+                         
+                                 nav button {
+                                     background-color: #4CAF50;
+                                     color: white;
+                                     padding: 10px 20px;
+                                     border: none;
+                                     border-radius: 5px;
+                                     cursor: pointer;
+                                     font-size: 16px;
+                                     margin-left: 15px;
+                                 }
+                         
+                                 nav button:hover {
+                                     background-color: #45a049;
+                                 }
+                         
+                                 #cards {
+                                     position: absolute;
+                                     top: 0;
+                                     left: 0;
+                                     width: 20%;
+                                     height: 95%;
+                                     background-color: #333333cc;
+                                     margin-top: 5vh;
+                                    \s
+                                 }
+                         
+                                 #card {
+                                     margin: 5%;
+                                     margin-top: 8%;
+                                     background-color: #232323;
+                                     color: white;
+                                     border-radius: 20px;
+                                     cursor: pointer;
+                                     padding: 5px;
+                                 }
+                         
+                                 #card p {
+                                     margin: 15px;
+                                     margin-top: 0%;
+                                     margin-bottom: 5px;
+                                     font-size: 12px;
+                                 }
+                         
+                                 #card h3 {
+                                     margin: 15px;
+                                     margin-bottom: 0%;
+                                     font-size: 15px;
+                                     margin-top: 8px;
+                                     display: inline;
+                                 }
+                         
+                                 .btn-detalhe {
+                                     background-color: #d5dcd500;
+                                     color: cadetblue;
+                                     border: none;
+                                     padding: 10px 15px;
+                                     border-radius: 100%;
+                                     cursor: pointer;
+                                     margin-left: 50%;
+                                     display: inline;
+                                 }
                     </style>
                 </head>
                                 
