dfc00fd5c4ad5d54d85ed1ad72104b827383d24c
lucas2651 <134lucasalves@gmail.com>
2024-02-02 18:17:21 -0300

    traduzi os metodos de pagamento

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/service/requests/gsonData/Method.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/service/requests/gsonData/Method.java
@@ -11,4 +11,22 @@ public class Method {
     private String type;
     private Card card;
     private Cash cash;
+
+    public String getMethod() {
+
+        return switch (method) {
+            case "CASH" -> "DINHEIRO";
+            case "CREDIT" -> "CRÉDITO";
+            case "DEBIT" -> "DÉBITO";
+            case "MEAL_VOUCHER"->"VALE REFEIÇÃO";
+            case "FOOD_VOUCHER"->"VALE ALIMENTAÇÃO";
+            case "GIFT_CARD"->"VALE-PRESENTE";
+            case "DIGITAL_WALLE"->"CARTEIRA DIGITA";
+            case "PIX"->"PIX";
+            default -> "não especificado";
+        };
+
+    }
 }
+
+
